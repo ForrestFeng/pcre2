45c969aa23dde931c1522fe259bdff8d767fbe04
Changzheng Feng <changzheng.feng@carestream.com>
2022-04-16 23:44:16 +0800

    pass build change pcre2_test.pcre2_test.bat use debug vesion to do test.

--- build/pcre2_test.bat
+++ build/pcre2_test.bat
@@ -5,6 +5,7 @@ SET srcdir="D:\Work\TableInsight\deps\pcre2"
 # The next line was replaced by the following one after a user comment.
 # SET pcre2test="$<TARGET_FILE:pcre2test>"
 SET pcre2test="D:\Work\TableInsight\deps\pcre2\build\pcre2test.exe"
+SET CMAKE_CONFIG_TYPE=Debug
 if not [%CMAKE_CONFIG_TYPE%]==[] SET pcre2test="D:\Work\TableInsight\deps\pcre2\build\%CMAKE_CONFIG_TYPE%\pcre2test.exe"
 call %srcdir%\RunTest.Bat
 if errorlevel 1 exit /b 1
